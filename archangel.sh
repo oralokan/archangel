25035225d5b28aa7850870f25f9cea0fb7328959
Oral Okan <oralokan@gmail.com>
2015-10-09 18:46:48 +0300

    Cleaner and more modular

--- archangel.sh
+++ archangel.sh
@@ -8,7 +8,27 @@
 # author: oral okan
 #   date: october, 2015
 
-CONTAINED_ITEM=""
+COUNTRY="Turkey"
+TIMEZONE="Europe/Istanbul"
+LOCALE="en_US.UTF-8"
+
+cat << EOM
+
+archangel -- installer script for Arch Linux
+
+Installs a fresh install of Arch Linux onto an entire hard drive.
+The disk is separated into two partitions:
+
+  1: The boot partition (512 MB)
+  2: Encrypted root partition (Rest of the available space)
+
+NOTE:
+In the current version, the following defaults are set automatically:
+    Installation Mirror: ftp.linux.org.tr
+    Time zone: Europe/Istanbul
+    Locale: en_US.UTF-8
+
+EOM
 
 function contains {
   local ITEM=$1
@@ -23,33 +43,35 @@ function contains {
   done
 }
 
-echo "archangel -- installer script for Arch Linux"
+function list_hdd {
+local LST=$(lsblk -pfln | grep '/dev/sd. ')
+cat << EOM
+Here is the list of available hard disk drives:
+EOM
+for ITEM in $LST
+do
+  printf "    $ITEM\n"
+done
 echo
-echo "NOTE: Currently, the time zone is set to Europe/Istanbul"
-echo "NOTE: Currently, the locale is always en_US.UTF-8"
-echo
-echo "Two partitions are created:"
-echo "  - 512 MB Boot Partition"
-echo "  - Rest goes to encrypted root partition"
-echo
+}
 
-# Begin by having the user select the target installation disk
-echo "Select the installation target disk."
-echo "IMPORTANT: All files on the disk will be deleted!"
-echo
-lsblk -pfln
-DEVICE_LIST=$(lsblk -pnr | awk -F' ' '{ print $1 }')
-echo
+function select_hdd {
+cat << EOM
+Pick the hard disk drive that Arch Linux will installed on.
+This should be of the form /dev/sdX (e.g. /dev/sda)
+WARNING: All the files on the selected device will be deleted!
 
-CONTAINED_ITEM=""
+EOM
+
+list_hdd  # List the available devices
+
+local DEVICE_LIST=$(lsblk -pnr | awk -F' ' '{ print $1 }')
+local CONTAINED_ITEM=""
 while [ -z "$CONTAINED_ITEM" ]
 do
-  echo "Pick the installation device."
-  echo "This should be of the form /dev/sdX"
   echo -n "Enter your selection:  "
   read SELECTED_DEVICE 
   contains "$SELECTED_DEVICE" "$DEVICE_LIST"
-  echo "result: $CONTAINED_ITEM"
   if [ -z "$CONTAINED_ITEM" ]
   then
     echo "ERROR: invalid selection!"
@@ -59,62 +81,149 @@ done
 DEVICE="$CONTAINED_ITEM"
 BOOT_PART=$DEVICE"1"
 ROOT_PART=$DEVICE"2"
+}
 
-# Get the hostname
-echo -n "Enter hostname:  "
-read HOSTNAME
+function get_hostname {
+  HOSTNAME=""
+  while [ -z "$HOSTNAME" ]
+  do
+    echo -n "Enter hostname:  "
+    read HOSTNAME
+    if [ -z "$HOSTNAME" ]
+    then
+      echo "Hostname cannot be empty"
+    fi
+  done
+}
 
-# Get disk password
-DISK_PASSWD=""
-while [ -z "$DISK_PASSWD" ]
-do
-  echo -n "Enter disk encryption password:  "
-  read -s DISK_PASSWD
-  echo
-  echo -n "Again...:  " 
-  read -s DISK_PASSWD_VFY
-  echo
-  if [ "$DISK_PASSWD" != "$DISK_PASSWD_VFY" ]
-  then
-    DISK_PASSWD="" 
-    echo "ERROR: passwords do not match!"
+function get_disk_pass {
+  DISK_PASSWD=""
+  while [ -z "$DISK_PASSWD" ]
+  do
+    echo -n "Enter disk encryption password:  "
+    read -s DISK_PASSWD
     echo
+    if [ -z "$DISK_PASSWD" ]
+    then
+      echo "Disk password cannot be empty" 
+      continue
+    fi
+    echo -n "Again...:  " 
+    read -s DISK_PASSWD_VFY
+    echo
+    if [ "$DISK_PASSWD" != "$DISK_PASSWD_VFY" ]
+    then
+      DISK_PASSWD="" 
+      echo "ERROR: passwords do not match!"
+      echo
+    fi
+  done
+}
+
+function get_root_pass {
+  ROOT_PASSWD=""
+  while [ -z "$ROOT_PASSWD" ]
+  do
+    echo -n "Enter root password:  "
+    read -s ROOT_PASSWD
+    echo
+    if [ -z "$ROOT_PASSWD" ]
+    then
+      echo "Root password cannot be empty" 
+      continue
+    fi
+    echo -n "Again...:  " 
+    read -s ROOT_PASSWD_VFY
+    echo
+    if [ "$ROOT_PASSWD" != "$ROOT_PASSWD_VFY" ]
+    then
+      ROOT_PASSWD="" 
+      echo "ERROR: passwords do not match!"
+      echo
+    fi
+  done
+}
+
+function get_boot_mode {
+  if [ -e "/sys/firmware/efi" ]
+  then 
+    BOOT_MODE="UEFI"  
+    BOOT_PART_LBL="gpt"
+    BOOT_PART_TYP="ESP"
+    BOOT_PART_FMT="fat32"
+    BOOT_PART_SIZE=512
+  else
+    BOOT_MODE="LEGACY"
+    BOOT_PART_LBL="msdos"
+    BOOT_PART_TYP="primary"
+    BOOT_PART_FMT="ext4"
+    BOOT_PART_SIZE=256
   fi
-done
+}
 
-# Figure out if we are in legacy or UEFI mode
-if [ -e "/sys/firmware/efi" ]
-then 
-  UEFI_MODE=1
-fi
+function get_country {
+  MIRROR_URL=""
+  while [ -z "$MIRROR_URL" ]
+  do
+    local COUNTRY=""
+    echo -n "Enter name of country of download mirror selection:  "
+    read COUNTRY
+    MIRROR_URL=$(grep -A1 $COUNTRY /etc/pacman.d/mirrorlist | tail -n1)
+    if [ -z "$MIRROR_URL" ]
+    then
+     echo "Mirror for country not found."
+     echo "Check /etc/pacman.d/mirrorlist"
+   fi
+  done
+}
 
+function get_configuration {
+  select_hdd 
+  get_hostname
+  get_country
+  get_disk_pass
+  get_root_pass
+  get_boot_mode
+}
 
-# GO/NOGO Decision
+function get_confirmation {
+cat << EOM
 
-echo
-echo "-------------------"
-echo "Go / No-Go Decision"
-echo "-------------------"
-echo
-echo "Target Device:  $DEVICE"
-echo "Boot Partition: $BOOT_PART"
-echo "Root Partition: $ROOT_PART (Encrypted)"
-if [ -n "$UEFI_MODE" ]
+Selected configuration:
+
+Target Disk:    $DEVICE
+Boot Mode:      $BOOT_MODE
+Boot Partition: $BOOT_PART  $BOOT_PART_SIZE MB  $BOOT_PART_TYP  $BOOT_PART_FMT 
+Root Partition: $ROOT_PART  LUKS (encrypted)
+LUKS Partition: /dev/mapper/cryptroot  primary  ext4
+Hostname:       $HOSTNAME
+Mirror URL:     $(echo $MIRROR_URL | awk -F" " '{ print $3 }')
+Timezone:       $TIMEZONE
+Locale:         $LOCALE
+
+WARNING: everything on $DEVICE will be deleted!!!
+
+EOM
+
+local CONFIRM=""
+
+echo -n "Type yes in uppercase if you want to continue...  "
+read CONFIRM
+
+if [ "$CONFIRM" != "YES" ]
 then
-  echo "Boot Mode:      UEFI"
-else
-  echo "Boot Mode:      Legacy"
+  echo "Goodbye"
+  exit
 fi
 
-echo "WARNING: everything on $DEVICE will be deleted!!!"
-echo
-echo -n "Type yes in uppercase if you want to continue..."
-read PROCEED
+}
 
-if [ "$PROCEED" != "YES" ]
-then
-    exit
-fi
+get_configuration
+get_confirmation
+
+echo "GO"
+
+exit
 
 ################################3
 
